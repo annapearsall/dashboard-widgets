e0e7093f0b2317b2a687fd666b3d6d71e8e560f2
Anna Pearsall <annakpearsall@gmail.com>
2022-04-28 08:44:10 -0700

    Add/format sunrise and sunset times
    get sunrise and sunset unix timestamps from api, then format into normal time

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,5 @@
 // API key. Replace with your API key
-const APIKEY = 'f2f85762267ef18d0df71e18ec9f3737';
+const APIKEY = 'ca4f7a2048f3631e2b48dfec851bfeff';
 // City
 const city = 'Spokane';
 // Units for Farenheit
@@ -19,12 +19,33 @@ fetch(url)
   // Get Container for Weather   
   const weatherContainer = document.querySelector('.weather');
   
+  // convert sunrise unix timestamp to normal time format
+  let sunriseUnix = `${data.sys.sunrise}`;
+
+  var sunriseDate = new Date(sunriseUnix * 1000);
+  var sunriseHours = sunriseDate.getHours();
+  var sunriseMinutes = "0" + sunriseDate.getMinutes();
+  const sunriseGotAmOrPM = sunriseHours >= 12 ? 'PM' : 'AM';
+  var sunriseFormattedTime = sunriseHours + ' : ' + sunriseMinutes.substr(-2) + ' ' + sunriseGotAmOrPM;
+
+  // convert sunset unix timestamp to normal time format
+  let sunsetUnix = `${data.sys.sunset}`;
+
+  var sunsetDate = new Date(sunsetUnix * 1000);
+  var sunsetHours = sunsetDate.getHours();
+  var sunsetMinutes = "0" + sunsetDate.getMinutes();
+  const sunsetGotAmOrPM = sunsetHours >= 12 ? 'PM' : 'AM';
+  var sunsetFormattedTime = sunsetHours + ' : ' + sunsetMinutes.substr(-2) + ' ' + sunsetGotAmOrPM;
+
+console.log(sunriseFormattedTime);
   // Template to output
   const template = `
     <h1>Weather</h1>
     <data value="${data.name}" class="city">${data.name}</data>
-    <data value="${data.main.temp}" class="temp">${data.main.temp}&#8457;</data>
     <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="Placeholder">
+    <data value="${data.main.temp}" class="temp">${data.main.temp}&#8457;</data> <br>
+    <data value="${data.sys.sunrise}" class="sunrise">Sunrise at ${sunriseFormattedTime}</data> <br>
+    <data value="${data.sys.sunset}" class="sunset">Sunset at ${sunsetFormattedTime}</data>
   `;
   
   // Insert dynamic template to container
